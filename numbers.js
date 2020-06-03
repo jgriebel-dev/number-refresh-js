3993660cd60af7619f3a36ffe186633c1bb805e4
Jefferson Griebel <jefferson.griebel@gmail.com>
2020-06-03 14:02:21 -0400

    parsing a string to an int

--- numbers.js
+++ numbers.js
@@ -1,5 +1,13 @@
-const secPerMin = 60;
+const secsPerMin = 60;
 const minsPerHr = 60;
 const HrPerDay = 24;
 const daysPerWeek = 7;
 const weeksPerYear = 52;
+
+
+// const secondsPerDay = secsPerMin * minsPerHr * HrPerDay;
+// console.log(`there are ${secondsPerDay} in day`);
+
+const yearsAlive = 29
+const secondsYears = secsPerMin * HrPerDay * weeksPerYear * yearsAlive
+console.log(`ive been alive more than ${secondsYears} seconds`)
---
+++ string-to-number.js
@@ -0,0 +1,7 @@
+const HTMLBadges = prompt('How man HTML badges do yoiu have?');
+const CSSBadges = prompt('How many CSS badged do you have?');
+
+const totalBadges = HTMLBadges + CSSBadges;
+console.log(totalBadges);
+console.log(typeof parseInt(HTMLBadges), typeof parseInt(CSSBadges));
+
